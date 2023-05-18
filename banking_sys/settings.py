99a925f6551eb56f7a0114bc59b5fe295f3897bf
web-weebie <twinklet969@gmail.com>
2023-05-18 11:42:18 +0100

    added static root

--- banking_sys/settings.py
+++ banking_sys/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+import dj_database_url
+import os
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +30,7 @@ SECRET_KEY = 'django-insecure-fu@e7x50fd3kmk2a+l#%6ai(1gifb4031(af@_iyhojr@9oh8r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -43,6 +48,7 @@ INSTALLED_APPS = [
     'transfer.apps.TransferConfig',
 
     'compressor',
+
 ]
 
 MIDDLEWARE = [
@@ -134,15 +140,16 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = [
-    BASE_DIR / 'static/'
-]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+
 
 COMPRESS_ROOT = BASE_DIR / 'static'
 
 COMPRESS_ENABLED = True
 
 STATICFILES_FINDERS = ('compressor.finders.CompressorFinder',)
+
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- banking_sys/urls.py
+++ banking_sys/urls.py
@@ -1,6 +1,8 @@
 
 from django.contrib import admin
 from django.urls import path, include 
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('home.urls')),
@@ -8,3 +10,5 @@ urlpatterns = [
     path('transfer/', include('transfer.urls')),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.COMPRESS_ROOT)
